1841d8cf2c03ca7a6ccceffe739723e99f135c0b
tomjo <Tom.Jorissen@cegeka.com>
2018-09-07 14:28:49 +0200

    Use hashes

--- src/main/java/com/cegeka/switchfully/security/SecurityConfig.java
+++ src/main/java/com/cegeka/switchfully/security/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.cegeka.switchfully.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
@@ -15,8 +17,11 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationEntryPoint authEntryPoint;
+    private final AuthenticationEntryPoint authEntryPoint;
+
+    public SecurityConfig(AuthenticationEntryPoint authEntryPoint) {
+        this.authEntryPoint = authEntryPoint;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -34,6 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().httpBasic()
                 .authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/cegeka/switchfully/security/external/authentication/FakeAuthenticationService.java
+++ src/main/java/com/cegeka/switchfully/security/external/authentication/FakeAuthenticationService.java
@@ -1,26 +1,46 @@
 package com.cegeka.switchfully.security.external.authentication;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+import static com.cegeka.switchfully.security.external.authentication.ExternalAuthenticaton.externalAuthenticaton;
 import static com.google.common.collect.Lists.newArrayList;
 
 @Service
 public class FakeAuthenticationService {
 
+    private final PasswordEncoder passwordEncoder;
+
+    public FakeAuthenticationService() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
     private List<ExternalAuthenticaton> externalAuthenticatons = newArrayList(
-            ExternalAuthenticaton.externalAuthenticaton().withUsername("ZWANETTA").withPassword("WORST").withRoles(newArrayList("CIVILIAN")),
-            ExternalAuthenticaton.externalAuthenticaton().withUsername("JMILLER").withPassword("THANKS").withRoles(newArrayList("PRIVATE")),
-            ExternalAuthenticaton.externalAuthenticaton().withUsername("UNCLE").withPassword("SAM").withRoles(newArrayList("HUMAN_RELATIONSHIPS")),
-            ExternalAuthenticaton.externalAuthenticaton().withUsername("GENNY").withPassword("RALLY").withRoles(newArrayList("GENERAL"))
+            externalAuthenticaton().withUsername("ZWANETTA")
+                    .withPassword("$2a$04$ogITLuIWMy5MeLPC6oLGT.AWOOTks94rZr47uk7obr9ChHR9h6OKa")
+                    .withRoles(newArrayList("CIVILIAN")),
+            externalAuthenticaton()
+                    .withUsername("JMILLER")
+                    .withPassword("$2a$04$w3MC6VpdKA8SpKd6tTk/beELHyXlEcY37uEQ5KLI1Ltv5Bv6edCiy")
+                    .withRoles(newArrayList("PRIVATE")),
+            externalAuthenticaton()
+                    .withUsername("UNCLE")
+                    .withPassword("$2a$04$aj/MmIEkn0n1xAaUmH0D4e7nDcvt31Me..qe8zQAokYawRd5XXFuq")
+                    .withRoles(newArrayList("HUMAN_RELATIONSHIPS")),
+            externalAuthenticaton()
+                    .withUsername("GENNY")
+                    .withPassword("$2a$04$EJD8pQH/02lMISqILWN.ZOL1y3me3FZgiFvkCmbXkd0bL0EFaVYnq")
+                    .withRoles(newArrayList("GENERAL"))
     );
 
     public Optional<ExternalAuthenticaton> getUser(String username, String password) {
         return externalAuthenticatons.stream()
                 .filter(externalAuthenticaton -> externalAuthenticaton.getUsername().equals(username))
-                .filter(externalAuthenticaton -> externalAuthenticaton.getPassword().equals(password))
+                .filter(externalAuthenticaton -> passwordEncoder.matches(password, externalAuthenticaton.getPassword()))
                 .findFirst();
     }
 }
